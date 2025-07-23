d63a7b46f972b5c7d17276a6d55bf3c88a830cac
Leticia-Sousa-DS <leticiasousassilva@hotmail.com>
2025-07-22 22:39:39 -0300

    feat: setup Design System with first Button component

---
+++ components/Button/Button.stories.ts
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react"
+import Button, { ButtonProps } from "./Button"
+
+const meta: Meta<ButtonProps> = {
+    title: 'Molecules/Button',
+    //tags: ['autodocs'],
+    component: Button,
+    argTypes: {}
+}
+
+export default meta 
+
+
+export const Primary: StoryObj<ButtonProps> = {
+    args:{
+        children: 'Botão'
+    }
+}
+
+export const Violet: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        className: 'theme-violet'
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+function toRgba(cssVar) {
+  const color = `var(${cssVar})`
+  return ({ opacityValue }) => `rgba(${color}, ${opacityValue})`
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      'mobile': '640px', 
+      'tablet': '1024px', 
+      'desktop': '1440px', 
+      'tv': '1920px', 
+    },
+    extend: {
+      colors: {
+        primary: toRgba('--primary'),
+        secondary: toRgba('--secondary'),
+        tertiary: toRgba('--tertiary'),
+        quaternary: toRgba('--quaternary'),
+        hover: toRgba('--hover'),
+        click: toRgba('--click'),
+        icon: toRgba('--icon'),
+        outline: toRgba('--outline'),
+        divider: toRgba('--divider'),
+        'bg-light': toRgba('--bg-light'),
+        'bg-dark': toRgba('--bg-dark'),
+        'bg-disabled': toRgba('--bg-disabled'),
+        'text-primary': toRgba('--text-primary'),
+        'text-secondary': toRgba('--text-secondary'),
+        'text-tertiary': toRgba('--text-tertiary'),
+        'text-disabled': toRgba('--text-disabled'),
+      },
+      boxShadow: {
+        sm: 'var(--shadow-sm)',
+        md: 'var(--shadow-md)',
+        lg: 'var(--shadow-lg)',
+        focus: 'var(--shadow-focus)',
+        outline: 'var(--shadow-outline)',
+        'button-focus': 'var(--shadow-button-focus)',
+      },
+      blur: {
+        default: 'var(--blur)',
+      },
+    },
+  },
+  plugins: [],
+}
