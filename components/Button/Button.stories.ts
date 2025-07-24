c67354ca88c48dca99b3242594390ca5505c4b62
Leticia-Sousa-DS <leticiasousassilva@hotmail.com>
2025-07-24 19:33:34 -0300

    docs: showcase additional Button variants to Storybook

--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -26,9 +26,62 @@ export const Primary: StoryObj<ButtonProps> = {
     }
 }
 
+export const Secondary: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        variant: 'secondary',
+    }
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        disabled: true,
+    }
+}
+
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        disabled: true,
+        variant: 'secondary',
+    }
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        variant: 'tertiary',
+    }
+}
+
+export const TertiaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'tertiary',
+    disabled: true
+  }
+}
+
 export const Violet: StoryObj<ButtonProps> = {
     args: {
         children: 'Botão',
         className: 'theme-violet'
+    }
+}
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        className: 'theme-violet',
+        variant: 'secondary',
+    }
+}
+
+export const VioletTertiary: StoryObj<ButtonProps> = {
+    args: {
+        children: 'Botão',
+        className: 'theme-violet',
+        variant: 'tertiary',
     }
 }
